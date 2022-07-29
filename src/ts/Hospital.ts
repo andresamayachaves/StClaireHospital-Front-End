a522512b71ff1c8a0b1e9126f25fb944b88ea133
andresamayachaves <andresamayachaves@gmail.com>
2022-07-29 18:30:47 -0500

    Front-end sketch finished

--- src/ts/Hospital.ts
+++ src/ts/Hospital.ts
@@ -1,1 +1,8 @@
-const var1:string = 'testing'
+const form: HTMLFormElement |null=
+document.querySelector('.hospital-form');
+
+form?.addEventListener("CreateANewSpecialty",handleCreate)
+
+function handleCreate(){
+    const createSpecialty = document.querySelector('.create-button') as HTMLButtonElement;
+}
